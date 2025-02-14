ca204f0b7ff9263e10eaa2b72ebe47314a637b2c
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-14 19:39:43 +0100

    Create ReportExporter.cs

---
+++ ReportExporter.cs
@@ -0,0 +1,17 @@
+using System.IO;
+using System.Text;
+
+public class ReportExporter : MonoBehaviour
+{
+    public void ExportToCSV(StudentReport report, string filePath)
+    {
+        var csv = new StringBuilder();
+        csv.AppendLine("Test,Score");
+        foreach (var test in report.testScores)
+        {
+            csv.AppendLine($"{test.Key},{test.Value}");
+        }
+        File.WriteAllText(filePath, csv.ToString());
+        Debug.Log($"Отчёт экспортирован в {filePath}");
+    }
+}
