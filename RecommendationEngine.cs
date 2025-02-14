76d952c0a961fe779a4308da6d8d4b0d2afa776b
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-14 19:51:50 +0100

    Create RecommendationEngine.cs

---
+++ RecommendationEngine.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using Unity.Barracuda; // Для работы с ONNX-моделями
+
+public class RecommendationEngine : MonoBehaviour
+{
+    public NNModel modelAsset;
+    private Model model;
+    private IWorker worker;
+
+    void Start()
+    {
+        model = ModelLoader.Load(modelAsset);
+        worker = WorkerFactory.CreateWorker(WorkerFactory.Type.Auto, model);
+    }
+
+    public string[] GetRecommendations(float[] inputFeatures)
+    {
+        Tensor inputTensor = new Tensor(1, inputFeatures.Length, inputFeatures);
+        worker.Execute(inputTensor);
+        Tensor outputTensor = worker.PeekOutput();
+        float[] scores = outputTensor.ToReadOnlyArray();
+
+        // Пример: выбор топ-3 тем
+        var recommendations = new List<string> { "math_advanced", "physics_quantum", "programming_basics" };
+        return recommendations.GetRange(0, 3).ToArray();
+    }
+
+    void OnDestroy()
+    {
+        worker.Dispose();
+    }
+}
