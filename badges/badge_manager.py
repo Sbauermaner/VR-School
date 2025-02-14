76752c5dd68f49c5c7eca239c696cf7eec638979
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-14 15:25:11 +0100

    Create badge_manager.py

---
+++ badges/badge_manager.py
@@ -0,0 +1,8 @@
+from .badge import Badge
+
+def create_default_badges():
+    return [
+        Badge("Новичок", "Первый шаг в VR"),
+        Badge("Исследователь", "Освоение базовых навыков"),
+        Badge("Мастер", "Продвинутые достижения")
+    ]
