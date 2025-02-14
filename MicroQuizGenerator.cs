926b29f79eba825ce0afa9729121c5d248405b9c
Sbauermaner <34747760+Sbauermaner@users.noreply.github.com>
2025-02-14 20:05:38 +0100

    Create MicroQuizGenerator.cs

---
+++ MicroQuizGenerator.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+
+public class MicroQuizGenerator : MonoBehaviour
+{
+    public QuizQuestion GenerateQuiz(List<string> topics)
+    {
+        // Пример: выбор случайного вопроса по теме
+        return new QuizQuestion
+        {
+            question = "Чему равен интеграл от x²?",
+            options = new List<string> { "x³/3", "x²/2", "2x" },
+            correctIndex = 0
+        };
+    }
+}
+
+[System.Serializable]
+public class QuizQuestion
+{
+    public string question;
+    public List<string> options;
+    public int correctIndex;
+}
